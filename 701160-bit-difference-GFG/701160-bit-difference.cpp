f5ff36a6dbec44f591245bc6d7f616b82c7a8c20
manjeet kumar <mkj22834@gmail.com>
2026-09-08 19:29:41 +0530

    Added solution - Time: O(log(max(a, b))), Space: O(1)

---
+++ 701160-bit-difference-GFG/701160-bit-difference.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int countBitsFlip(int a, int b) {
+        // code here
+        int num = a ^ b;
+        // count the set bits of num
+        int count = 0;
+        while(num){
+            if(num & 1) count++;
+            num = num>>1;
+        }
+        return count;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
