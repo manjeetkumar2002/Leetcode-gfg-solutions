6c237791a81dc6221235db0de63c2cb26ef05779
manjeet kumar <mkj22834@gmail.com>
2026-09-08 19:19:47 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 701154-check-kth-bit-GFG/701154-check-kth-bit.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    bool checkKthBit(int n, int k) {
+        //  code here
+        return n & (1<<k);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
