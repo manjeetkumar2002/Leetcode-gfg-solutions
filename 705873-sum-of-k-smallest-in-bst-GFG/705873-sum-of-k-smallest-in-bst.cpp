1a3065c7b2e759d90ab962d661468037a2d8d54f
manjeet kumar <mkj22834@gmail.com>
2026-09-08 17:08:26 +0530

    Added solution - Time: O(N), Space: O(H)

---
+++ 705873-sum-of-k-smallest-in-bst-GFG/705873-sum-of-k-smallest-in-bst.cpp
@@ -0,0 +1,42 @@
+/* Structure of a Tree Node
+class Node {
+    int data;
+    Node* right;
+    Node* left;
+    Node(int x){
+        data = x;
+        right = nullptr;
+        left = nullptr;
+    }
+}; */
+
+class Solution {
+  public:
+  
+    void find(Node*root,int& k,int& sum){
+        if(!root) return;
+        
+        
+        find(root->left,k,sum);
+        k--;
+        if(k>=0){
+            sum+=root->data;
+        }
+        else{
+            return;
+        }
+        
+        find(root->right,k,sum);
+    }
+    int sum(Node* root, int k) {
+        // code here
+        int sum = 0;
+        find(root,k,sum);
+        
+        return sum;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
