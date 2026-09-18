45150d7a1374918ecf0c8d2a3e994d42a4e83054
manjeet kumar <mkj22834@gmail.com>
2026-09-18 18:26:28 +0530

    Added solution - Time: O(N log N), Space: O(N)

---
+++ 700552-fixing-two-nodes-of-bst-GFG/700552-fixing-two-nodes-of-bst.cpp
@@ -0,0 +1,46 @@
+/* Structure of a Binary Search Tree node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+}; */
+
+class Solution {
+  public:
+  void inorder(Node *root, vector<int> &ans){
+      if (!root)
+          return;
+
+      inorder(root->left, ans);
+      ans.push_back(root->data);
+      inorder(root->right, ans);
+  }
+  void fixtree(Node*root,vector<int>& arr,int & index){
+       if (!root)
+          return;
+
+      fixtree(root->left, arr,index);
+      root->data = arr[index++];
+      fixtree(root->right, arr,index);
+  }
+    Node* correctBST(Node* root) {
+        // code here
+        vector<int> tree;
+        inorder(root,tree);
+        sort(tree.begin(),tree.end());
+        // fix the tree
+        int index =0;
+        fixtree(root,tree,index);
+        return root;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
