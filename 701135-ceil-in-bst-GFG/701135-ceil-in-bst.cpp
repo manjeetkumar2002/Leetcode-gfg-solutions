b86009eba2c7b98dbc260f454e30b813b49b3c03
manjeet kumar <mkj22834@gmail.com>
2026-09-19 13:38:59 +0530

    Added solution - Time: O(h), Space: O(1)

--- 701135-ceil-in-bst-GFG/701135-ceil-in-bst.cpp
+++ 701135-ceil-in-bst-GFG/701135-ceil-in-bst.cpp
@@ -15,24 +15,22 @@ class Node {
 
 class Solution {
   public:
-  void inorder(Node * root,vector<Node*> &ans){
-          if(!root) return;
 
-          inorder(root->left,ans);
-          ans.push_back(root);
-          inorder(root->right,ans);
-      }
     int findCeil(Node* root, int x) {
         // code here
-        vector<Node *> ans;
-        inorder(root,ans);
         int ceil = -1;
-        
-        for(int i=0;i<ans.size();i++){
-            if(ans[i]->data>=x){
-                return ans[i]->data;
+        while(root){
+            if(root->data==x) return x;
+            
+            else if(root->data<x){
+                root = root->right;
+            }
+            else{
+                ceil = root->data;
+                root = root->left;
             }
         }
+        
         return ceil;
     }
 };
