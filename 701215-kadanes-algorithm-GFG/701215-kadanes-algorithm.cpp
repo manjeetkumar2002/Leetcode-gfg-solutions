43714b2ae533b45c8e78ec9e43d836e2737800ca
manjeet kumar <mkj22834@gmail.com>
2026-09-21 15:54:26 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 701215-kadanes-algorithm-GFG/701215-kadanes-algorithm.cpp
@@ -0,0 +1,24 @@
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(vector<int> &arr) {
+        long long prefix=0;
+        long long maxi=INT_MIN;
+        
+        for(int i=0;i<arr.size();i++){
+            prefix+=arr[i];
+            maxi=max(maxi,prefix);
+            
+            if(prefix<0){
+                prefix=0;
+            }
+        }
+        return maxi;
+    }
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
